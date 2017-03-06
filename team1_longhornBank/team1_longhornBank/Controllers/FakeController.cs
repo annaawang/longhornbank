803d40bbe7b4f3cc644cfbc261588ae5b97c9bf4
Brian Peterson <bap2428@utexas.edu>
2017-03-06 15:01:41 -0600

    added fake controller

---
+++ team1_longhornBank/team1_longhornBank/Controllers/FakeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace team1_longhornBank.Controllers
+{
+    public class FakeController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
